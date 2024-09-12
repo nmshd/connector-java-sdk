f8bc6b519c7334f1e14f2e754960e916046fb79d
Max Baboshin <c-maksym.baboshyn@hulu.com>
2024-09-12 12:10:08 +0200

    Remove RelationshipAttribute and change successorContent to attribute

--- src/main/java/eu/enmeshed/model/notificationItem/PeerSharedAttributeSucceededNotificationItem.java
+++ src/main/java/eu/enmeshed/model/notificationItem/PeerSharedAttributeSucceededNotificationItem.java
@@ -1,7 +1,6 @@
 package eu.enmeshed.model.notificationItem;
 
-import eu.enmeshed.model.attributes.IdentityAttribute;
-import eu.enmeshed.model.attributes.RelationshipAttribute;
+import eu.enmeshed.model.attributes.Attribute;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
@@ -10,8 +9,7 @@ import lombok.experimental.SuperBuilder;
 @Setter
 @SuperBuilder
 public class PeerSharedAttributeSucceededNotificationItem extends NotificationItem {
-  private IdentityAttribute successorContent;
-  private RelationshipAttribute attribute;
+  private Attribute successorContent;
   private String predecessorId;
   private String successorId;
 }
