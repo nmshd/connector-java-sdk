36d7951a4c4b06d4d92550b2ab571507860d9425
Julian König <julian.koenig@js-soft.com>
2024-09-17 13:46:50 +0200

    feat: implement challenges

--- src/main/java/eu/enmeshed/ConnectorClient.java
+++ src/main/java/eu/enmeshed/ConnectorClient.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import eu.enmeshed.endpoints.AccountEndpoint;
 import eu.enmeshed.endpoints.AttributesEndpoint;
+import eu.enmeshed.endpoints.ChallengesEndpoint;
 import eu.enmeshed.endpoints.FilesEndpoint;
 import eu.enmeshed.endpoints.IncomingRequestsEndpoint;
 import eu.enmeshed.endpoints.MessagesEndpoint;
@@ -20,18 +21,15 @@ import feign.form.FormEncoder;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 
+@SuppressWarnings("ClassCanBeRecord")
 public class ConnectorClient {
 
-  private static final ObjectMapper objectMapper =
-      new ObjectMapper()
-          .registerModule(new JavaTimeModule())
-          .setSerializationInclusion(Include.NON_ABSENT)
-          .disable(SerializationFeature.INDENT_OUTPUT)
-          .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-          .setDateFormat(new StdDateFormat().withColonInTimeZone(true));
+  private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule()).setSerializationInclusion(Include.NON_ABSENT)
+      .disable(SerializationFeature.INDENT_OUTPUT).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).setDateFormat(new StdDateFormat().withColonInTimeZone(true));
 
   public final AccountEndpoint account;
   public final AttributesEndpoint attributes;
+  public final ChallengesEndpoint challenges;
   public final FilesEndpoint files;
   public final MessagesEndpoint messages;
   public final RelationshipTemplatesEndpoint relationshipTemplates;
@@ -42,14 +40,17 @@ public class ConnectorClient {
   public ConnectorClient(
       AccountEndpoint account,
       AttributesEndpoint attributes,
+      ChallengesEndpoint challenges,
       FilesEndpoint files,
       MessagesEndpoint messages,
       RelationshipTemplatesEndpoint relationshipTemplates,
       RelationshipsEndpoint relationships,
       IncomingRequestsEndpoint incomingRequests,
-      OutgoingRequestsEndpoint outgoingRequests) {
+      OutgoingRequestsEndpoint outgoingRequests
+  ) {
     this.account = account;
     this.attributes = attributes;
+    this.challenges = challenges;
     this.files = files;
     this.messages = messages;
     this.relationshipTemplates = relationshipTemplates;
@@ -59,28 +60,28 @@ public class ConnectorClient {
   }
 
   public static ConnectorClient create(String url, String apiKey) {
-    return create(url, apiKey, new Options(), Level.NONE);
+    return create(url, apiKey, new Options(), Level.FULL);
   }
 
-  public static ConnectorClient create(
-      String url, String apiKey, Options options, Level loggerLevel) {
-    var builder =
-        Feign.builder()
-            .decoder(new JacksonDecoder(objectMapper))
-            .encoder(new FormEncoder(new JacksonEncoder(objectMapper)))
-            .requestInterceptor(request -> request.header("X-API-KEY", apiKey))
-            .logLevel(loggerLevel)
-            .options(options)
-            .errorDecoder(new ConnectorErrorDecoder());
+  public static ConnectorClient create(String url, String apiKey, Options options, Level loggerLevel) {
+    var builder = Feign.builder()
+        .decoder(new JacksonDecoder(objectMapper))
+        .encoder(new FormEncoder(new JacksonEncoder(objectMapper)))
+        .requestInterceptor(request -> request.header("X-API-KEY", apiKey))
+        .logLevel(loggerLevel)
+        .options(options)
+        .errorDecoder(new ConnectorErrorDecoder());
 
     return new ConnectorClient(
         AccountEndpoint.configure(url, builder),
         AttributesEndpoint.configure(url, builder),
+        ChallengesEndpoint.configure(url, builder),
         FilesEndpoint.configure(url, builder),
         MessagesEndpoint.configure(url, builder),
         RelationshipTemplatesEndpoint.configure(url, builder),
         RelationshipsEndpoint.configure(url, builder),
         IncomingRequestsEndpoint.configure(url, builder),
-        OutgoingRequestsEndpoint.configure(url, builder));
+        OutgoingRequestsEndpoint.configure(url, builder)
+    );
   }
 }
