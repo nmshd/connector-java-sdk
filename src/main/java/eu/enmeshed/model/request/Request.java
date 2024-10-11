3abea729b6d0c5d7d6c094bc08668eba96ac656f
Julian König <julian.koenig@js-soft.com>
2024-10-11 12:30:34 +0200

    fix: use Builder.Default

--- src/main/java/eu/enmeshed/model/request/Request.java
+++ src/main/java/eu/enmeshed/model/request/Request.java
@@ -5,6 +5,7 @@ import eu.enmeshed.model.messages.MessageContent;
 import eu.enmeshed.model.request.requestItems.RequestItem;
 import java.time.ZonedDateTime;
 import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,6 +18,7 @@ import lombok.experimental.SuperBuilder;
 public class Request extends MessageContent {
 
   @JsonProperty("@type")
+  @Builder.Default
   private String type = "Request";
   private String id;
   private String title;
