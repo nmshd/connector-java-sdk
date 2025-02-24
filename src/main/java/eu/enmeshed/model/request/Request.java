6b2c04338af918518b04ce83b0c5bbbc3f2f2520
DmitryKazinets <dmitri.kazinec@wisercat.net>
2025-02-24 16:00:18 +0300

    Fix relationship feign headers & request model (#20)

--- src/main/java/eu/enmeshed/model/request/Request.java
+++ src/main/java/eu/enmeshed/model/request/Request.java
@@ -1,20 +1,31 @@
 package eu.enmeshed.model.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import eu.enmeshed.model.messages.MessageContent;
 import eu.enmeshed.model.request.requestItems.RequestItem;
 import java.time.ZonedDateTime;
 import java.util.List;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+
 @NoArgsConstructor
 @Getter
 @Setter
 @SuperBuilder
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "@type",
+    visible = true,
+    defaultImpl = Request.class
+)
 public class Request extends MessageContent {
 
   @JsonProperty("@type")
