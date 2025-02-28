3b8db7a21ea82787d41174c862b173a8454ef428
Dmitry Kazinets <dmitri.kazinec@wisercat.net>
2025-02-28 15:56:12 +0300

    Removed NON_EMPTY

--- src/main/java/eu/enmeshed/requests/incomingRequests/AcceptRequestItem.java
+++ src/main/java/eu/enmeshed/requests/incomingRequests/AcceptRequestItem.java
@@ -1,6 +1,5 @@
 package eu.enmeshed.requests.incomingRequests;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
 import lombok.AllArgsConstructor;
@@ -9,7 +8,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
