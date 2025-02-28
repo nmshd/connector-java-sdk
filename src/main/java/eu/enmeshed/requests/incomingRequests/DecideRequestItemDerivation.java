cec7053401ca3bdac31490a6a759e463dbb4c729
Dmitry Kazinets <dmitri.kazinec@wisercat.net>
2025-02-28 15:35:27 +0300

    Added permits for DecideRequestItemDerivation

--- src/main/java/eu/enmeshed/requests/incomingRequests/DecideRequestItemDerivation.java
+++ src/main/java/eu/enmeshed/requests/incomingRequests/DecideRequestItemDerivation.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
-public sealed class DecideRequestItemDerivation extends DecideRequestItem permits RejectRequestItem, AcceptRequestItem {
+public sealed class DecideRequestItemDerivation extends DecideRequestItem permits RejectRequestItem, AcceptRequestItem, AcceptFreeTextRequestItem {
 
   @JsonProperty("accept")
   private boolean accept;
