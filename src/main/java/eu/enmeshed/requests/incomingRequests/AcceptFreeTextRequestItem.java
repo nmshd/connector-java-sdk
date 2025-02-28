5851719944914e2190a22eab85e5d47649064bb1
DmitryKazinets <dmitri.kazinec@wisercat.net>
2025-02-28 16:00:03 +0300

    Added AcceptFreeTextRequestItem & AcceptRequestItem NON_EMPTY (#23)
    * Added AcceptFreeTextRequestItem & AcceptRequestItem NON_EMPTY
    
    * Added permits for DecideRequestItemDerivation
    
    * Removed NON_EMPTY

---
+++ src/main/java/eu/enmeshed/requests/incomingRequests/AcceptFreeTextRequestItem.java
@@ -0,0 +1,22 @@
+package eu.enmeshed.requests.incomingRequests;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public final class AcceptFreeTextRequestItem extends DecideRequestItemDerivation {
+    @JsonProperty("accept")
+    private final boolean accept = true;
+    private String freeText;
+}
